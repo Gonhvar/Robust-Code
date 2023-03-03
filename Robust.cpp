b63a55631cf2fd2e0fd046cfbae4cb5d9800de39
Gonhvar <126863031+Gonhvar@users.noreply.github.com>
2023-03-03 16:11:25 +0100

    Add files via upload

--- Robust.cpp
+++ Robust.cpp
@@ -16,13 +16,16 @@ using namespace std;
 #define W_1B 0x2F
 #define W_2B 0x2B
 #define W_4B 0x23
-#define R_1B 0x40
+#define R 0x40
+#define R_1B 0x4F
+#define R_2B 0x4B
+#define R_4B 0x43
 
 //Valeur de base de la sortie des messages CanOpen
 TPCANHandle channelUsed = PCAN_USBBUS1;
 TPCANBaudrate baudrateUsed = PCAN_BAUD_1M;
 
-//================INITIALISATION================
+//================INITIALISATION_PEAK================
 TPCANHandle find_channel(){
     TPCANHandle channelsToCheck[] = { PCAN_USBBUS1, PCAN_USBBUS2, PCAN_USBBUS3, PCAN_USBBUS4, PCAN_USBBUS5, PCAN_USBBUS6
      , PCAN_USBBUS7, PCAN_USBBUS8, PCAN_USBBUS9, PCAN_USBBUS10, PCAN_USBBUS11, PCAN_USBBUS12, PCAN_USBBUS13, PCAN_USBBUS14
@@ -65,7 +68,7 @@ TPCANHandle find_channel(){
 
 TPCANBaudrate init_baudrate_doc(){
 
-    TPCANBaudrate baudrates[] = { PCAN_BAUD_125K, PCAN_BAUD_250K, PCAN_BAUD_500K, PCAN_BAUD_1M };
+    TPCANBaudrate baudrates[] = { PCAN_BAUD_1M, PCAN_BAUD_500K, PCAN_BAUD_250K, PCAN_BAUD_125K};
     DWORD listenOnlyState = PCAN_PARAMETER_ON;
     TPCANMsg msg;
     printf("Checking the current bitrate on channel 0x%X\n", channelUsed);
@@ -143,7 +146,7 @@ void init_msg_SDO(TPCANMsg* msg, int id, uint8_t data_length,uint8_t index_1, ui
     msg->MSGTYPE = PCAN_MESSAGE_STANDARD;
     msg->LEN = 8;
 
-    //On copie chacune des valeurs de msg_d dans msg->DATA
+    //On copie chacune des valeurs de msg_data dans msg->DATA
 
     msg->DATA[0] = data_length;
     msg->DATA[1] = index_2;
@@ -183,43 +186,76 @@ void init_msg_SDO(TPCANMsg* msg, int id, uint8_t data_length,uint8_t index_1, ui
     }
 }
 
-void print_message(TPCANMsg received){
+uint32_t print_message(TPCANMsg received){
 
-    uint32_t result;
+    uint32_t result = -1;
     
-    printf("\n+++++++++++++++++\n");
-    printf("Index : 0x%hhx-%hhx | Sub-Index : 0x%hhx\n", received.DATA[2], received.DATA[1], received.DATA[3]);
+    //Si ce n'est pas un message renvoyé aprés ecriture
+    if(received.DATA[0]!=0x60){
+        printf("\n+++++++++++++++++\n");
+        printf("Index : 0x%hhx-%hhx | Sub-Index : 0x%hhx\n", received.DATA[2], received.DATA[1], received.DATA[3]);
 
-    switch(received.DATA[0]){
-        case W_1B :
-            //1 octet
-            result = received.DATA[4];
-            printf("Data hexa : 0x%hhx\n",received.DATA[4]);
-            printf("Data deci : %d", result);
-            break;
+        switch(received.DATA[0]){
+            case R_1B :
+                //1 octet
+                result = received.DATA[4];
+                printf("Data hexa : 0x%hhx\n",received.DATA[4]);
+                printf("Data deci : %d", result);
+                break;
 
-        case W_2B :
-            //2 octets
-            result = (received.DATA[5]<<8) | received.DATA[4];
-            printf("Data hexa : 0x%hhx-%hhx\n",received.DATA[5],received.DATA[4]);
-            printf("Data deci : %d", result);
-            break;
+            case R_2B :
+                //2 octets
+                result = (received.DATA[5]<<8) | received.DATA[4];
+                printf("Data hexa : 0x%hhx | %hhx\n",received.DATA[5],received.DATA[4]);
+                printf("Data deci : %d", result);
+                break;
 
-        case W_4B :
-            //4 octets
-            result = (received.DATA[7]<<24) | (received.DATA[6]<<16)| (received.DATA[5]<<8) | received.DATA[4];
-            printf("Data hexa : 0x%hhx-%hhx-%hhx-%hhx\n",received.DATA[7],received.DATA[6],received.DATA[5],received.DATA[4]);
-            printf("Data decimal : %d", result); 
-            break;
-        
-        case 0x60 : 
-            printf("Message renvoyé aprés ecriture");
-            break;
+            case R_4B :
+                //4 octets
+                result = (received.DATA[7]<<24) | (received.DATA[6]<<16)| (received.DATA[5]<<8) | received.DATA[4];
+                printf("Data hexa : 0x%hhx | %hhx | %hhx | %hhx\n",received.DATA[7],received.DATA[6],received.DATA[5],received.DATA[4]);
+                printf("Data decimal : %d", result); 
+                break;
+                
+            default : 
+                printf("Erreur taille du message : 0x%hhx\n", received.DATA[0]);
+        }
+        printf("\n+++++++++++++++++\n");
             
-        default : 
-            printf("Erreur taille du message\n");
+    }else{
+        printf("Message confirmation de Index : 0x%hhx-%hhx | Sub-Index : 0x%hhx\n", received.DATA[2], received.DATA[1], received.DATA[3]);
+        //printf("Data : %hhx | %hhx | %hhx | %hhx", received.DATA[7], received.DATA[6], received.DATA[5], received.DATA[4]);
+        return 0;
     }
-    printf("\n+++++++++++++++++\n");
+    return result;
+}
+
+uint32_t read_message(){
+
+    TPCANMsg received;
+    TPCANStatus result;
+    TPCANTimestamp timestamp;
+    char strMsg[256];
+
+    uint8_t i=0;
+    uint32_t get;
+
+    do
+    {
+        // Check the receive queue for new messages
+        //
+        result = CAN_Read(channelUsed,&received,&timestamp);
+        //Si ce n'est pas vide 
+        if(result != PCAN_ERROR_QRCVEMPTY)
+        {
+            //Un message et reçue et on l'affiche :
+            get = print_message(received);
+            //printf("Le message est arrivé en %dms\n", timestamp.millis);
+            //printf("msg type : %hhx\n", received.MSGTYPE);
+        }
+    //Jusqu'a ce que la file d'attente soit vide
+    }while((result & PCAN_ERROR_QRCVEMPTY) != PCAN_ERROR_QRCVEMPTY);
+    return get;
 }
 
 void write_message(TPCANMsg msg){
@@ -227,7 +263,7 @@ void write_message(TPCANMsg msg){
     TPCANStatus result;
     char strMsg[256];
 
-    // The message is sent using the PCAN-USB Channel 1
+    // The message is sent using the PCAN-USB Channel
     result = CAN_Write(channelUsed, &msg);
     if(result != PCAN_ERROR_OK)
     {
@@ -238,120 +274,83 @@ void write_message(TPCANMsg msg){
     }
     else{
         //printf("Message sent successfully\n");
+        read_message();
     }
 
     //En micro seconde :
-    usleep(1000);
-    //printf("--------\n");
+    usleep(600);
 }
 
-void read_message(){
-
-    TPCANMsg received;
-    TPCANStatus result;
-    TPCANTimestamp timestamp;
-    char strMsg[256];
-
-    do
-    {
-        // Check the receive queue for new messages
-        //
-        result = CAN_Read(channelUsed,&received,&timestamp);
-        if(result != PCAN_ERROR_QRCVEMPTY)
-        {
-            // Process the received message
-            //
-            //printf("msg type : %hhx\n", received.MSGTYPE);
-            //printf("Le message est arrivé en %dms\n", timestamp.millis);
-            print_message(received);
-
-        }
-        else
-        {
-            // An error occurred, get a text describing the error and show it
-            // and handle the error
-            //
-            CAN_GetErrorText(result, 0, strMsg);
-            printf("%s\n", strMsg);
-            // Here can be decided if the loop has to be  terminated (eg. the bus
-            // status is  bus-off)
-            //
-        }
-    // Try to read a message from the receive queue of the PCAN-USB, Channel 1,
-    // until the queue is empty
-    //
-    }while((result & PCAN_ERROR_QRCVEMPTY) != PCAN_ERROR_QRCVEMPTY);
-
-    printf("--------\n");
-}
-
-void get_value(TPCANMsg toSend){
+uint32_t get_value(TPCANMsg toSend){
     //----------------------------------
     //DATA[0] Toujours égale à 0x40 si on veut read une valeur
-    toSend.DATA[0] = R_1B;
+    toSend.DATA[0] = R;
     write_message(toSend);
 
     //-------------------------------------------
     usleep(1000);  //En micro-secondes
     //-------------------------------------------
 
-    read_message();
+    return (read_message());
 }
-//==================INIT_CONTROL_POSITION==================
+
+//==================POSITION MODE==================
 
 void def_positionAbsolue(int id){
     TPCANMsg msg;
-    uint8_t msg_d[4];
-    bzero(msg_d, 4);
+    uint8_t msg_data[4];
+    bzero(msg_data, 4);
 
     //Modes of operation (Homing Mode)
-    msg_d[0] = 0x06;
-    init_msg_SDO(&msg, id, W_1B, 0x60, 0x60, 0x00, msg_d);
+    msg_data[0] = 0x06;
+    init_msg_SDO(&msg, id, W_1B, 0x60, 0x60, 0x00, msg_data);
     write_message(msg);
 
     //Homing method
-    msg_d[0] = 0x25;
-    init_msg_SDO(&msg, id, R_1B, 0x60, 0x98, 0x00, msg_d);
+    msg_data[0] = 0x25; //=37d
+    init_msg_SDO(&msg, id, W_1B, 0x60, 0x98, 0x00, msg_data);
     write_message(msg);
 
     //Controlword (Shutdown)
-    msg_d[0] = 0x00;
-    msg_d[1] = 0x06;
-    init_msg_SDO(&msg, id, W_2B, 0x60, 0x40, 0x00, msg_d);
+    msg_data[0] = 0x00;
+    msg_data[1] = 0x06;
+    init_msg_SDO(&msg, id, W_2B, 0x60, 0x40, 0x00, msg_data);
     write_message(msg);
+
+    usleep(1000);
 
     //Controlword (Switch on & Enable)
-    msg_d[0] = 0x00;
-    msg_d[1] = 0x0F;
-    init_msg_SDO(&msg, id, W_2B, 0x60, 0x40, 0x00, msg_d);
+    msg_data[0] = 0x00;
+    msg_data[1] = 0x0F;
+    init_msg_SDO(&msg, id, W_2B, 0x60, 0x40, 0x00, msg_data);
     write_message(msg);
 
+    usleep(1000);
+
     //Controlword (Start homing)
-    msg_d[0] = 0x00;
-    msg_d[1] = 0x1F;
-    init_msg_SDO(&msg, id, W_2B, 0x60, 0x40, 0x00, msg_d);
+    msg_data[0] = 0x00;
+    msg_data[1] = 0x1F;
+    init_msg_SDO(&msg, id, W_2B, 0x60, 0x40, 0x00, msg_data);
     write_message(msg);
 
     //Controlword (Halt homing)
-    msg_d[0] = 0x01;
-    msg_d[1] = 0x1F;
-    init_msg_SDO(&msg, id, W_2B, 0x60, 0x40, 0x00, msg_d);
+    msg_data[0] = 0x01;
+    msg_data[1] = 0x1F;
+    init_msg_SDO(&msg, id, W_2B, 0x60, 0x40, 0x00, msg_data);
     write_message(msg);
 }
 
-void init_asservissementPosition(int id){
+bool init_asservissementPosition(int id){
 
     TPCANMsg msg;
-    uint8_t msg_d[4];
-    bzero(msg_d, 4);
+    uint8_t msg_data[4];
+    bzero(msg_data, 4);
 
-    printf("Debut positionAbsolue\n");
     def_positionAbsolue(id);
-    printf("Fin positionAbsolue\n");
 
     //---------Modes of operation
-    msg_d[0] = 0x01;
-    init_msg_SDO(&msg, id, W_1B, 0x60, 0x60, 0x00, msg_d);
+    msg_data[0] = 0x01; //profil position mode
+    init_msg_SDO(&msg, id, W_1B, 0x60, 0x60, 0x00, msg_data);
     write_message(msg);
 
     //---------Set parameter : fait avec EPOSSTUDIO
@@ -359,51 +358,75 @@ void init_asservissementPosition(int id){
     //
     //---------Enable device
     //Controlword (shutdown)
-    msg_d[0] = 0;
-    msg_d[1] = 0x06;
-    init_msg_SDO(&msg, id, W_2B, 0x60, 0x40, 0x00, msg_d);
+    msg_data[0] = 0x00;
+    msg_data[1] = 0x06;
+    init_msg_SDO(&msg, id, W_2B, 0x60, 0x40, 0x00, msg_data);
     write_message(msg);
 
-    //Controlword (shutdown)
-    msg_d[0] = 0;
-    msg_d[1] = 0x0F;
-    init_msg_SDO(&msg, id, W_2B, 0x60, 0x40, 0x00, msg_d);
+    usleep(1000);
+
+    //Controlword (Switch on and Enable)
+    msg_data[0] = 0x00;
+    msg_data[1] = 0x0F;
+    init_msg_SDO(&msg, id, W_2B, 0x60, 0x40, 0x00, msg_data);
     write_message(msg);
 
+    return false;
 }
 
-void set_relativePosition(int id, uint8_t userInput){
+void set_relativePosition(int id, uint32_t userInput){
     TPCANMsg msg;
-    uint8_t msg_d[4];
-    bzero(msg_d, 4);
+    uint8_t msg_data[4];
+    bzero(msg_data, 4);
 
+    //Controlword (New Position)
+    msg_data[0] = 0x00;
+    msg_data[1] = 0x0F;
+    init_msg_SDO(&msg, id, W_2B, 0x60, 0x40, 0x00, msg_data);
+    write_message(msg);
+
+    if(userInput>0xFFFF){
+        userInput = 0xFFFF;
+    }
+
+    printf("UserInput : %hhx\n %d\n", userInput, userInput);
+    
     //Target position
-    msg_d[0] = userInput;
-    msg_d[1] = 0;
-    msg_d[2] = 0;
-    msg_d[3] = 0;
-    init_msg_SDO(&msg, id, W_4B, 0x60, 0x7A, 0x00, msg_d);
+    //On limite à 2 Bytes
+    msg_data[0] = (userInput>>8)%8;
+    msg_data[1] = userInput;
+    
+    printf("UserInput : 0x%hhx-%hhx\n", msg_data[0], msg_data[1]);
+    init_msg_SDO(&msg, id, W_2B, 0x60, 0x7A, 0x00, msg_data);
     write_message(msg);
 
     //Controlword (relative position)
-    msg_d[0] = 0;
-    msg_d[0] = 0x5F;
-    init_msg_SDO(&msg, id, W_2B, 0x60, 0x4A, 0x00, msg_d);
+    msg_data[0] = 0;
+    msg_data[1] = 0x5F;
+    init_msg_SDO(&msg, id, W_2B, 0x60, 0x40, 0x00, msg_data);
     write_message(msg);
 
-    /*//Controlword (New Position)
-    msg_d[0] = 0;
-    msg_d[0] = 0x0F;
-    init_msg_SDO(&msg, id, W_2B, 0x60, 0x40, 0x00, msg_d);
+    /*//ABSOLUE
+    msg_data[0] = 0x00;
+    msg_data[1] = 0x1F;
+    init_msg_SDO(&msg, id, W_2B, 0x60, 0x40, 0x00, msg_data);
     write_message(msg);*/
+
+
+    //Attente obligé 
+    usleep(1000);
 }
 
-void AsservissementPosition(int id){
+void set_position(int id){
     uint32_t userInput = 1;
-    
-    printf("Debut setup\n");
-    init_asservissementPosition(id);
-    printf("Fin setup\n");
+    TPCANMsg msg;
+    uint8_t msg_data[4];
+    bzero(msg_data, 4);
+    bool wait= true;
+
+    wait = init_asservissementPosition(id);
+
+    while(wait);
 
     while(userInput != 0){
         printf("Donnez une valeur en decimal pour la position (0 pour quitter): \n");
@@ -411,35 +434,115 @@ void AsservissementPosition(int id){
         set_relativePosition(id, userInput);
         usleep(1000);
     }
+
+    msg_data[0] = 0x01;
+    msg_data[1] = 0x0F;
+    init_msg_SDO(&msg, id, W_2B, 0x60, 0x40, 0x00, msg_data);
+    write_message(msg);
+
     printf("Fin du programme : \n");
+}
+
+//==================TORQUE MODE==================
+
+void init_Torque(int id){
+    TPCANMsg msg;
+    uint8_t msg_data[4];
+    bzero(msg_data, 4);
+
+    //Modes of operation (Cyclic Synchronous Torque Mode)
+    msg_data[0] = 0x0A;
+    init_msg_SDO(&msg, id, W_1B, 0x60, 0x60, 0x00, msg_data);
+    write_message(msg);
+
+    //Controlword (Shutdown)
+    msg_data[0] = 0x00;
+    msg_data[1] = 0x06;
+    init_msg_SDO(&msg, id, W_1B, 0x60, 0x40, 0x00, msg_data);
+    write_message(msg);
+}
+
+void set_torque(int id){
+
+    uint32_t userInput = 1;
+    uint32_t userInput2 = 1;
+    
+    TPCANMsg msg;
+    uint8_t msg_data[4];
+    bzero(msg_data, 4);
+
+    init_Torque(id);
+
+    printf("\nDonnez une valeur en decimal pour le offset du couple (en pourcent) : \n");
+    cin >> userInput;
+    
+
+    msg_data[0] = userInput;
+    msg_data[1] = userInput>>8;
+    init_msg_SDO(&msg, id, W_2B, 0x60, 0xB2, 0x00, msg_data);
+    write_message(msg);
+
+    while(userInput != 0){
+        printf("\nDonnez une valeur en decimal pour le couple (en pourcent) \n(0 pour quitter): \n");
+        cin >> userInput2;
+
+        msg_data[0] = userInput2>>8;
+        msg_data[1] = userInput2;
+        init_msg_SDO(&msg, id, W_2B, 0x60, 0x71, 0x00, msg_data);
+        write_message(msg);
+
+        usleep(1000);
+    }
+
+    //QuickStop
+    msg_data[0] = 0x00;
+    msg_data[1] = 0x0B;
+    init_msg_SDO(&msg, id, W_2B, 0x60, 0x40, 0x00, msg_data);
+    write_message(msg);
+
+    printf("Fin du programme : \n");
+}
+
+//==================MENU==================
+
+void mode_selection(int id){
+    int userInput = 8;
+    do{
+        printf("Quel mode voulez vous lancer ?\n 0.Quitter\n 1.Controle en Position\n 2.Controle en Couple\n 3.Controle en Vitesse\n");
+        cin >> userInput;
+    }while(userInput>3);
+
+    switch (userInput){
+        case 0 : printf("Quitte\n"); 
+        break;
+
+        case 1 : 
+        set_position(id);
+        break;
+
+        case 2 : 
+        set_torque(id);
+        break;
+
+        case 3 :
+        printf("Pas encore implémenté\n");
+        break;
+
+        default : 
+            printf("Valeur invalide\n");
+    }
 }
 
 //==================MAIN==================
 int main(){
-    //Initialisation des variables 
-    TPCANMsg msg;
-    uint8_t msg_d[4];
-    bzero(msg_d, 4);
 
-    printf("=======DEBUT PROGRAMME=======\n");
     //Init Dynamique des valeurs de channel et de baudrate 
     channelUsed = find_channel();
-    baudrateUsed = init_baudrate_doc();
+    //Remettre a la fin
+    //baudrateUsed = init_baudrate_doc();
 
     //Initialisation du PEAK
     initialise_CAN_USB();
 
-    //Pour le controle en position
-    AsservissementPosition(COBID_CAN1_SDO);
-    
-    /*
-    msg_d[0] = 0x25;
-    msg_d[1] = 0xFF;
-    init_msg_SDO(&msg,COBID_CAN1_SDO, W_2B, 0x60, 0x7F, 0x60, msg_d);
-    print_message(msg);
-    
-    //On receptionne le message
-    read_message();
-    */
-    printf("=======FIN PROGRAMME=======\n");
+    mode_selection(COBID_CAN3_SDO);
 }
